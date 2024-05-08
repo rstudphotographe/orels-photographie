e62071b34ec4b4492a401c11d3af6d09d45ecea8
peters <roselpeters@yahoo.fr>
2024-05-08 16:38:55 +0200

    ajout

--- stud.js
+++ stud.js
@@ -1,3 +1,23 @@
+const buttons = document.querySelectorAll(".btn");
+const slides = document.querySelectorAll(".slide");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const calcNextSlide = e.target.id === "next" ? 1 : -1;
+    const slideActive = document.querySelector(".active");
+
+    newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+
+    if (newIndex < 0) newIndex = [...slides].length - 1;
+    if (newIndex >= [...slides].length) newIndex = 0;
+    slides[newIndex].classList.add("active");
+    slideActive.classList.remove("active");
+  });
+});
+
+
+
+
 document.querySelectorAll('.parent img').forEach(image =>{
   image.onclick = () => {
     document.querySelector('.popup-image').style.display = 'block';
